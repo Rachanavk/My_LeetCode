f0feebcd7b9bffe67133effd56c6b1ae592a9ac3
Rachanavk <65361077+Rachanavk@users.noreply.github.com>
2023-12-04 22:21:07 -0500

    Create minStack.py

---
+++ Stack/minStack.py
@@ -0,0 +1,34 @@
+# MinStack
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int val) pushes the element val onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+from collections import deque
+class MinStack:
+
+    def __init__(self):
+        self.container = deque()
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.container.append(val)
+        value = min(val,self.minStack[-1] if self.minStack else val)
+        self.minStack.append(value)
+
+    def pop(self) -> None:
+        self.container.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.container[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
